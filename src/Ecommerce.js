998b3fe8cba29113e3b20cab9aecb2eacab018e3
Hossain <hossain.ie2402@gmail.com>
2023-01-05 22:25:16 +0600

    Added the comercial websites database things

--- src/Ecommerce.js
+++ src/Ecommerce.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {db} from "./firebase.js";
 import Home from "./Home.js"
 import "./Ecommerce.css"
@@ -6,31 +7,56 @@ import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 
 function Ecommerce() {
 
- 	return (
+      const [datas,setDatas] = useState([]);
+
+
+      const addItem = ()=>{
+            datas.push("laptop");
+            console.log(datas);
+      }
+      const add = ()=>{
+            db.collection("order_datas").add({data: datas});
+            setDatas([]);
+      }
+
+
+
+      return (
             <div className="Ecommerce">
-                  <center><div className="add_to_cart_btn">Add to cart</div></center>
+                  <center><div className="add_to_cart_btn" onClick={add}>Add to cart</div></center>
 
 
                   <div className="products">
                         <div className="product p1">
-                              <div className="add">Add</div>
+                              <div className="add a1" onClick = { addItem }>Add</div>
                         </div>
                         <div className="product p2">
-                              <div className="add">Add</div>
+                              <div className="add a2" onClick = { addItem }>Add</div>
                         </div>
                         <div className="product p3">
-                              <div className="add">Add</div>
+                              <div className="add a3" onClick = { addItem }>Add</div>
                         </div>
-                        <div className="product p4">
-                              <div className="add">Add</div>
+                        <div className="product p3">
+                              <div className="add a3" onClick = { addItem }>Add</div>
                         </div>
-                        <div className="product p5">
-                              <div className="add">Add</div>
+                  </div>
+                  <div className="products">
+                        <div className="product p1">
+                              <div className="add a1" onClick = { addItem }>Add</div>
+                        </div>
+                        <div className="product p2">
+                              <div className="add a2" onClick = { addItem }>Add</div>
+                        </div>
+                        <div className="product p3">
+                              <div className="add a3" onClick = { addItem }>Add</div>
+                        </div>
+                        <div className="product p3">
+                              <div className="add a3" onClick = { addItem }>Add</div>
                         </div>
                   </div>
 
             </div>
-	  );
-	}
+        );
+      }
 
 export default Ecommerce;
